ee46d9dba90a80bea28d3e771dd6b955e1fea479
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-06-21 12:31:49 +0530

    factor common chains out of tests

---
+++ src/test/scala/TestChains.scala
@@ -0,0 +1,7 @@
+trait TestChains {
+  val rootOnly = new Blockchain()
+  val length2chain =
+    rootOnly.mineBlock(Seq(Transaction("vecna", "tiamat", 1)), "vecna", Seq("vecna", "tiamat"))
+  val length3chain = length2chain.mineBlock(Seq(Transaction("tiamat", "vecna", 1)), "tiamat")
+  val length4chain = length3chain.mineBlock(Seq(Transaction("vecna", "tiamat", 1)), "tiamat")
+}
