1f7f30acc1c29d67aae246c3396c0635dd1f8da0
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-07-16 21:27:23 +0200

    get one block sync over networking working

---
+++ src/main/scala/Serializer.scala
@@ -0,0 +1,12 @@
+package ratan.blockchain
+
+import akka.util.ByteString
+
+object Serializer {
+  def serialize(x: Serializable): ByteString =
+    ByteString(org.apache.commons.lang3.SerializationUtils.serialize(x))
+
+  def deserialize(x: ByteString): Any =
+    org.apache.commons.lang3.SerializationUtils.deserialize(x.toArray)
+
+}
