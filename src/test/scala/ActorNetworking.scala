f9baf67d602a5238c4a04794f0f422df708434d3
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-09-28 11:52:18 -0400

    peer discovery

--- src/test/scala/ActorNetworking.scala
+++ src/test/scala/ActorNetworking.scala
@@ -3,12 +3,14 @@ package raicoin
 import akka.actor._
 import akka.testkit.TestProbe
 import org.scalatest._
-import java.net.InetAddress
+import java.net.InetSocketAddress
 import scala.concurrent.duration._
 import scala.concurrent.Await
 import BlockchainActor._
 import Serializer._
 import akka.io.Tcp
+
+import TestUtils._
 
 class ActorNetworking extends FunSuiteLike with TestChains {
 
@@ -26,10 +28,38 @@ class ActorNetworking extends FunSuiteLike with TestChains {
     implicit val defaultSender = p.testActor
 
     Thread.sleep(1000)
-    actorB ! Request(3)
-    p.expectMsg(1.seconds, Tcp.Write(serialize(length4chain(3))))
+    retriesOnTimeout(1) {
+      actorB ! Request(3)
+      p.expectMsg(1.seconds, Tcp.Write(serialize(length4chain(3))))
+    }
     Await.result(systemB.terminate(), Duration.Inf)
     Await.result(systemA.terminate(), Duration.Inf)
+  }
+
+  test("B and C discover each other through A") {
+
+      val systemA = ActorSystem("A")
+      val actorA  = systemA.actorOf(Props(new BlockchainActor(length4chain, None)), "A")
+
+      val systemB = ActorSystem("B")
+      val actorB  = systemB.actorOf(Props(new BlockchainActor(length4chain)), "B")
+
+      Thread.sleep(1000)
+
+      val systemC = ActorSystem("C")
+      val actorC  = systemC.actorOf(Props(new BlockchainActor(length4chain)), "C")
+
+      val p                      = TestProbe("p")(systemC)
+      implicit val defaultSender = p.testActor
+
+      Thread.sleep(500)
+      retriesOnTimeout(1) {
+        actorC ! GetPeers
+        p.receiveN(2)
+      }
+      Await.result(systemC.terminate(), Duration.Inf)
+      Await.result(systemB.terminate(), Duration.Inf)
+      Await.result(systemA.terminate(), Duration.Inf)
   }
 
 }
--- src/test/scala/TestUtils.scala
+++ src/test/scala/TestUtils.scala
@@ -7,4 +7,15 @@ object TestUtils {
   def tcpWritten(obj: Serializable) = {
     Tcp.Write(serialize(obj))
   }
+
+  def retriesOnTimeout[T](n: Int)(block: =>T): T = {
+    require(n >= 0)
+    try {
+      block
+    } catch {
+      case ae: AssertionError => if (ae.getMessage.contains("timeout") && n > 0) {
+        retriesOnTimeout(n-1)(block)
+      } else throw ae
+    }
+  }
 }
