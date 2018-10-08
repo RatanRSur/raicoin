9a65470cde1f050907283b9485bccd184bc4be49
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-10-08 15:55:57 -0400

    test bigger network propogation

--- src/test/scala/ActorNetworking.scala
+++ src/test/scala/ActorNetworking.scala
@@ -59,4 +59,34 @@ class ActorNetworking extends FunSuiteLike with TestChains {
       Seq(systemA, systemB, systemC).foreach(system => Await.result(system.terminate(), Duration.Inf))
   }
 
+  test("changes propagate through a bigger system") {
+
+    val systemA = ActorSystem("A")
+    val actorA  = systemA.actorOf(Props(new BlockchainActor(length4chain, None)), "A")
+
+    val systemB = ActorSystem("B")
+    val actorB  = systemB.actorOf(Props(new BlockchainActor(rootOnly)), "B")
+
+    val systemC = ActorSystem("C")
+    val actorC  = systemC.actorOf(Props(new BlockchainActor(rootOnly)), "C")
+
+    val p                      = TestProbe("p")(systemC)
+    implicit val defaultSender = p.testActor
+
+    actorC ! GetPeerInfo
+    val cInfo = deserialize(p.receiveN(1).head.asInstanceOf[Tcp.Write].data).asInstanceOf[PeerInfo]
+
+    val systemD = ActorSystem("D")
+    val actorD  = systemD.actorOf(Props(new BlockchainActor(rootOnly, Some(cInfo))), "D")
+
+    Thread.sleep(1000)
+    retriesOnTimeout(1) {
+      actorD ! Request(3)
+      p.expectMsg(1.seconds, Tcp.Write(serialize(length4chain(3))))
+    }
+
+    Seq(actorA, actorB, actorC, actorD).foreach(_ ! Disconnect)
+    Seq(systemA, systemB, systemC, systemD).foreach(system => Await.result(system.terminate(), Duration.Inf))
+  }
+
 }
