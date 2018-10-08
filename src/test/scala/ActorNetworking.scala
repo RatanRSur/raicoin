852f8dd6f4d5c918b0c75eab2993b4a7a7828bdd
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-10-08 15:55:57 -0400

    test bigger network propogation

--- src/test/scala/ActorNetworking.scala
+++ src/test/scala/ActorNetworking.scala
@@ -51,7 +51,36 @@ class ActorNetworking extends FunSuiteLike with TestChains {
     systemC.terminate()
     systemB.terminate()
     systemA.terminate()
-    //fail
+  }
+
+  test("changes propagate through a bigger system") {
+
+    val systemA = ActorSystem("A")
+    val actorA  = systemA.actorOf(Props(new BlockchainActor(length4chain, None)), "A")
+
+    val systemB = ActorSystem("B")
+    val actorB  = systemB.actorOf(Props(new BlockchainActor(rootOnly)), "B")
+
+    val systemC = ActorSystem("C")
+    val actorC  = systemC.actorOf(Props(new BlockchainActor(rootOnly)), "C")
+
+    val p                      = TestProbe("p")(systemC)
+    implicit val defaultSender = p.testActor
+
+    actorC ! GetPeerInfo
+    val cInfo = deserialize(p.receiveN(1).head.asInstanceOf[Tcp.Write].data).asInstanceOf[PeerInfo]
+
+    val systemD = ActorSystem("D")
+    val actorD  = systemD.actorOf(Props(new BlockchainActor(rootOnly, Some(cInfo))), "D")
+
+    Thread.sleep(1000)
+    actorD ! Request(3)
+    val snarf = p.expectMsg(1.seconds, Tcp.Write(serialize(length4chain(3))))
+
+    systemD.terminate()
+    systemC.terminate()
+    systemB.terminate()
+    systemA.terminate()
   }
 
 }
