db048ea7145622b7eea1303eeeef0441a0af37a3
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-10-09 14:10:10 -0400

    remove Dependencies.scala

--- project/Dependencies.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object Dependencies {
-  lazy val scalaTest     = "org.scalatest"      %% "scalatest"    % "3.0.5"
-  lazy val akka          = "com.typesafe.akka"  %% "akka-actor"   % "2.5.12"
-  lazy val akkaTest      = "com.typesafe.akka"  %% "akka-testkit" % "2.5.12" % Test
-  lazy val apacheCommons = "org.apahce.commons" % "commons-codec" % "1.11"
-}
