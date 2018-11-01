e4f5949df868315d592251944634db6cc5395df8
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-11-01 12:10:39 -0400

    wip

---
+++ src/main/scala/Raicoin.scala
@@ -0,0 +1,76 @@
+package raicoin
+
+import scala.io.StdIn._
+import scorex.crypto._
+import scorex.crypto.signatures._
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FilenameUtils
+import java.io.File
+import java.nio.file.Paths
+
+object Raicoin {
+  val keyBasename = "raicoin"
+  val privateKeyName = s"$keyBasename.priv"
+  val publicKeyName = s"$keyBasename.pub"
+
+  def main(args: Array[String]): Unit = {
+    println(asciiArt)
+    println("Public Private Key Pair")
+    println("[G]enerate or [P]rovide as file?")
+    val keyGenerationNeeded = "Gg".contains(getCharOneof("GgPp"))
+    val (privateKey, publicKey) = if (keyGenerationNeeded) {
+      val (privKey, pubKey): (PrivateKey, PublicKey) = Curve25519.createKeyPair
+      print("Directory to save raicoin.pub, raicoin.priv: ")
+      val directory = readDirectory()
+      val (privateKeyFile, publicKeyFile) =
+        (new File(directory, privateKeyName),
+         new File(directory, publicKeyName))
+      FileUtils.writeByteArrayToFile(privateKeyFile, privKey)
+      FileUtils.writeByteArrayToFile(publicKeyFile, pubKey)
+      loadPrivateAndPublicKey(directory)
+    } else {
+      print(s"Directory where $privateKeyName and $publicKeyName are saved: ")
+      loadPrivateAndPublicKey(readDirectory())
+    }
+
+  }
+
+  def readDirectory(): String = {
+    val pathSep = Option(System.getProperty("file.separator")).get
+    val inputWithPathSep = readLine().stripSuffix(pathSep) + pathSep
+
+    /*expand home dir if needed*/
+    if (inputWithPathSep.startsWith("~/")) {
+      Option(System.getProperty("user.home")).get + inputWithPathSep.stripPrefix("~")
+    } else inputWithPathSep
+  }
+
+  def loadPrivateAndPublicKey(directoryName: String): (PrivateKey, PublicKey) = {
+    (FileUtils.readFileToByteArray(
+       new File(directoryName, privateKeyName)).asInstanceOf[PrivateKey],
+     FileUtils.readFileToByteArray(
+       new File(directoryName, privateKeyName)).asInstanceOf[PublicKey])
+  }
+
+  def getCharOneof(validChars: String): Char = {
+    val keypress = readChar()
+    if (validChars.contains(keypress)) keypress
+    else {
+      println(s"Invalid choice. Choose one of: [$validChars]")
+      getCharOneof(validChars)
+    }
+  }
+
+  val asciiArt: String = """
+
+  ooooooooo.              oOo                       oOo
+  `OOO   `YOO.            `"'                       `"'
+   OOO   .dOO'  .oooo.   oooo   .ooooo.   .ooooo.  oooo  ooo. .oo.
+   OOOoooOOP'  `P  )OOb  `OOO  dOO' `"YO dOO' `OOb `OOO  `OOOP"YOOb
+   OOO`OOb.     .oP"OOO   OOO  OOO       OOO   OOO  OOO   OOO   OOO
+   OOO  `OOb.  dO(  OOO   OOO  OOO   .oO OOO   OOO  OOO   OOO   OOO
+  oOOOo  oOOOo `YOOO""Oo oOOOo `YObodOP' `YObodOP' oOOOo oOOOo oOOOo
+
+                                                                  """
+
+}
